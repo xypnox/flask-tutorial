ae7c50e5422052d99f64ccf4211a8ec771b8a19a
xypnox <xypnox@gmail.com>
2021-05-24 22:44:47 +0530

    Base

--- app.py
+++ app.py
@@ -1,44 +1,167 @@
 from flask import Flask, jsonify, request
-from todx import fabric
 import json
-import hashlib
+from random import randint
 
 app = Flask(__name__)
+
+MAX_REWARD = 20
+
+data = {}
+
+with open("data.json") as file:
+    data = json.loads(file.read())
+
+print(data['parlors'])
+
+
+# Functions
+
 
 @app.route('/', methods=['GET'])
 def index():
     with open('data.json') as file:
         return file.read()
 
-@app.route('/hash', methods = ['POST'])
-def hash():
+
+# Parlor Endpoints
+@app.route('/parlor/<pid>', methods=['GET'])
+def parlor_id(pid):
     """
-    Requires a request of the format:
-    {
-            'hash' : 'HASH'
-    }
-    and returns whether the file has changed or not
+    Return Parlor details by ID
     """
-    if request.headers['Content-Type'] == 'application/json':
-        data = request.json
-        print(type(data))
-        with open('data.json') as dataFile:
-            readfile = dataFile.read().encode('utf8')
-            md5Hash = hashlib.md5(readfile)
-            md5Hashed = md5Hash.hexdigest()
-            print(md5Hashed)
-            if data['hash'] == md5Hashed:
-                return jsonify(
-                    {
-                        'status':'matched'
-                    }
-                )
-            else:
-                return jsonify(
-                    {
-                        'status':'changed'
-                    }
-                )
+    for p in data['parlors']:
+        if pid == p['id']:
+            return jsonify(p)
+
+    return jsonify(
+        {
+            'ERROR': 'NOT FOUND'
+        }
+    )
+
+
+@app.route('/parlor/<pid>/transactions', methods=['GET'])
+def parlor_transactions():
+    """
+    """
+    return jsonify(
+        {
+            'status': 'changed'
+        }
+    )
+
+
+# Machine Endpoints
+@app.route('/machine/<mid>/', methods=['GET'])
+def machine(mid):
+    """
+    Return Machine details
+    """
+    for m in data['machines']:
+        if mid == m['id']:
+            return jsonify(m)
+    return jsonify(
+        {
+            'ERROR': 'NOT FOUND'
+        }
+    )
+
+
+@app.route('/machine/<mid>/play/<game_id>', methods=['POST'])
+def machine_play(mid, game_id):
+    """
+    Perform transaction, make machine occupied
+    Fail with error
+    """
+    return jsonify(
+        {
+            'status': 'changed'
+        }
+    )
+
+
+@app.route('/machine/<mid>/stop', methods=['POST'])
+def machine_stop(mid):
+    """
+    Return the Random reward as well
+    """
+    for i, m in enumerate(data['machines']):
+        if mid == m['id']:
+            data['machines'][i]['status'] = 'available'
+
+            return jsonify({'machine': data['machines'][i], 'reward': MAX_REWARD})
+
+    return jsonify(
+        {
+            'status': 'changed'
+        }
+    )
+
+
+# User Routes
+@app.route('/user/<uid>/', methods=['GET'])
+def user(uid):
+    """
+    Return User details
+    """
+    for u in data['users']:
+        if uid == u['id']:
+            return jsonify(u)
+    return jsonify(
+        {
+            'ERROR': 'NOT FOUND'
+        }
+    )
+
+
+@app.route('/user/<uid>/transactions', methods=['GET'])
+def user_transactions(uid):
+    """
+    """
+    return jsonify(
+        {
+            'status': 'changed'
+        }
+    )
+
+
+@app.route('/user/<uid>/card', methods=['GET'])
+def user_card(uid):
+    """
+    Current user card info
+    """
+    for u in data['users']:
+        if uid == u['id']:
+            return jsonify(u['card'])
+    return jsonify(
+        {
+            'ERROR': 'NOT FOUND'
+        }
+    )
+
+
+@app.route('/transaction/<tid>', methods=['GET'])
+def transaction(tid):
+    """
+    Return User details
+    """
+    for t in data['transactions']:
+        if tid == t['id']:
+            return jsonify(t)
+    return jsonify(
+        {
+            'ERROR': 'NOT FOUND'
+        }
+    )
+
+
+@app.route('/transactions', methods=['GET'])
+def transactions():
+    """
+    Return User details
+    """
+    return jsonify(data['transactions'])
+
 
 if __name__ == '__main__':
     app.run()
---
+++ models.py
@@ -0,0 +1,12 @@
+class Parlor:
+    def __init__(self, pid, name, earnings, location):
+        self.id = pid
+        self.name = name
+        self.earnings = earnings
+        self.location = location
+
+    def getDetails(self):
+        return "Details of Parlor: []"
+
+    def getTransactions(self):
+        return "Transactions: []"
