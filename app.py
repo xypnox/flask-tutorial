c75b055e378ac2a0e51e281a1350dd1625a54a9e
xypnox <xypnox@gmail.com>
2021-05-24 23:34:34 +0530

    F sub

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import json
 from random import randint
+from datetime import date
 
 app = Flask(__name__)
 
@@ -15,6 +16,87 @@ print(data['parlors'])
 
 
 # Functions
+def playMachine(mid, gid, uid):
+
+    for i, m in enumerate(data['machines']):
+        if mid == m['id']:
+
+            if m['status'] == 'occupied':
+                return jsonify({'ERROR': 'MACHINE OCCUPIED'})
+
+            amount = getGamPrice(mid, gid)
+            print(amount)
+            if isNotPlayable(uid, amount):
+                return({'ERROR': 'INSUFFICIENT BALANCE'})
+
+            addMoney(uid, -amount)
+
+            data['machines'][i]['status'] = 'occupied'
+
+            t = {
+                "id": "t"+str(len(data['transactions'])+1),
+                "type": "game",
+                "itemId": gid,
+                "amount": amount,
+                "pid": getParlorID(mid),
+                "uid": uid,
+                "mid": mid
+            }
+            #addEarnings(getParlorID(mid), amount)
+
+            data['transactions'].append(t)
+
+            return jsonify({'machine': data['machines'][i], 'transaction': t})
+
+    return jsonify({'ERROR': 'NOT FOUND'})
+
+
+def stopMachine(mid, uid):
+
+    for i, m in enumerate(data['machines']):
+        if mid == m['id']:
+            data['machines'][i]['status'] = 'available'
+            reward = randint(0, MAX_REWARD)
+            addMoney(uid, reward)
+            return jsonify({'machine': data['machines'][i], 'reward': reward})
+
+    return jsonify({'ERROR': 'NOT FOUND'})
+
+
+def addMoney(uid, amount):
+    for i, u in enumerate(data['users']):
+        if uid == u['id']:
+            data['users'][i]['card']['amount'] += amount
+
+
+def getGamPrice(mid, gid):
+    for i, m in enumerate(data['machines']):
+        if mid == m['id']:
+            for i, g in enumerate(m['games']):
+                if gid == g['id']:
+                    return g['price']
+    return -1
+
+
+def isNotPlayable(uid, amount):
+    today = str(date.today())
+    # print("Today's date:", today)
+    if amount < 0:
+        return True
+    else:
+        for u in data['users']:
+            if uid == u['id']:
+                if u['card']['daily'] == today:
+                    return False
+                elif u['card']['amount'] > amount:
+                    return False
+
+    return True
+
+
+def getParlorID(mid):
+    return 'p1'
+# Routes
 
 
 @app.route('/', methods=['GET'])
@@ -73,11 +155,9 @@ def machine_play(mid, game_id):
     Perform transaction, make machine occupied
     Fail with error
     """
-    return jsonify(
-        {
-            'status': 'changed'
-        }
-    )
+    uid = request.json["uid"]
+
+    return playMachine(mid, game_id, uid)
 
 
 @app.route('/machine/<mid>/stop', methods=['POST'])
@@ -85,17 +165,9 @@ def machine_stop(mid):
     """
     Return the Random reward as well
     """
-    for i, m in enumerate(data['machines']):
-        if mid == m['id']:
-            data['machines'][i]['status'] = 'available'
+    uid = request.json["uid"]
 
-            return jsonify({'machine': data['machines'][i], 'reward': MAX_REWARD})
-
-    return jsonify(
-        {
-            'status': 'changed'
-        }
-    )
+    return stopMachine(mid, uid)
 
 
 # User Routes
@@ -140,6 +212,7 @@ def user_card(uid):
     )
 
 
+# Transactions
 @app.route('/transaction/<tid>', methods=['GET'])
 def transaction(tid):
     """
